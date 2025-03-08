89c816e1dabcd7c7319ac2fecb5127c8c647d9c9
Miriyan Asenov <mirorim.789@gmail.com>
2025-03-08 11:56:42 +0200

    3. Modified - App.jsx. New - Input.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import {useState} from 'react';
 import Info from './components/Info';
-import Button from './components/Button'; 
+import Button from './components/Button';
+import Input from './components/Input';
 
-function App() {
-    const [secNum, setSecNum] = useState( Math.trunc( Math.random() * 100 ) + 1 );
-    const secNumInfo = secNum + ' is the secret number';
+function App(){
+  const [secNum, setSecNum] = useState( Math.trunc( Math.random() * 100 ) + 1 );
+  const [inp, setInp] = useState('');
+  const [guess, setGuess] = useState(null);
+  const secNumInfo = guess === null ? 'Guess the number' : guess === secNum ? 'Yes' : 'No - ' + secNum;
+
+  function handleChange(e){
+    setInp( e.target.value ); 
+  }
   
-    function handleNewGame(){
-      setSecNum( Math.trunc( Math.random() * 100 ) + 1 );
-    }
-  
-    return (
-      <>
-        <Info message={secNumInfo} />
-        <Button handleOnClick={handleNewGame}>New Game</Button>
-      </>
-    );
+  function handleCheck(){
+    setGuess( Number(inp) );
+  }
+
+  function handleNewGame(){
+    setSecNum( Math.trunc( Math.random() * 100 ) + 1 );
+    setInp('');
+    setGuess(null);
+  }
+
+  return (
+    <>
+      <Info message={secNumInfo} />
+      <Input inpVal={inp} handleOnChange={handleChange}/>
+      <Button handleOnClick={handleCheck}>Check</Button>
+      <Button handleOnClick={handleNewGame}>New Game</Button>
+    </>
+  );
 }
 
 export default App;
